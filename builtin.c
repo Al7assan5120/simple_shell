2a148e8ebee130bbeb0b836abf606d3902111bad
Al7assan <al7assan.r@gmail.com>
2023-09-23 04:49:41 +0300

    c 0.6

--- builtin.c
+++ builtin.c
@@ -8,24 +8,21 @@ int is_builtin(char **f)
 {
 	char *arr [] = {"exit", "cd", "env"};
 	int i;
-	
+
 	for (i = 0; i < 3 ; i++)
 	{
 		if (_strcmp(f[0], arr[i]) == 0)
 			break;
 	}
-	
 	switch (i)
 	{
 	case 0:
-		return(1);
-		break;
+		return (1);
 	case 1:
-		/*x = chdir (f[1]);*/
+		chdir(f[1]);
 		break;
 	case 2:
-		return(3);
-		break;
+		return (3);
 	}
 	return (0);
 }
